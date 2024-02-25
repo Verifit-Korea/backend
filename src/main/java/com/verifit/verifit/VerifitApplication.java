57e26119761ff684c1eed580b00ce6b6d6ff2a7e
mark1346 <markymark331@gmail.com>
2024-02-25 23:05:56 +0900

    Init

---
+++ src/main/java/com/verifit/verifit/VerifitApplication.java
@@ -0,0 +1,13 @@
+package com.verifit.verifit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerifitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerifitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/verifit/verifit/VerifitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.verifit.verifit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VerifitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
