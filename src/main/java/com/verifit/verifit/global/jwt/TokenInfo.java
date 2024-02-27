73739b0d00e406031a95ccf56d8f897b0d155f05
ktk8916 <ktk8916@naver.com>
2024-02-27 21:19:21 +0900

    jwt provider

---
+++ src/main/java/com/verifit/verifit/global/jwt/TokenInfo.java
@@ -0,0 +1,54 @@
+package com.verifit.verifit.global.jwt;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Getter
+public class TokenInfo implements UserDetails {
+
+    private Long id;
+
+    @Builder
+    public TokenInfo(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
