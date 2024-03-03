35d4d0d4171b99a6b2f529fb589810872e4d05a4
JinhyeokBang <jinhyeokfang@gmail.com>
2024-03-04 00:24:51 +0900

    feat: 도메인 및 레파지토리 구현 #2 회원 기능에 필요한 엔티티 (도메인 로직), 레파지토리 구현, Spring Validation 의존성 추가

---
+++ src/main/java/com/verifit/verifit/member/dao/MemberRepository.java
@@ -0,0 +1,16 @@
+package com.verifit.verifit.member.dao;
+
+import com.verifit.verifit.member.entity.Member;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    @Override
+    Optional<Member> findById(Long id);
+
+    Optional<Member> findByEmail(String email);
+
+    boolean existsByNickname(String nickname);
+
+    boolean existsByEmail(String email);
+}
