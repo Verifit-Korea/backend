c2de05421db7c40856babf819bdcee6c0282e113
ktk8916 <ktk8916@naver.com>
2024-03-15 20:22:46 +0900

    refactor : 신규회원, 기존회원 로그인 분기 처리

--- src/main/java/com/verifit/verifit/auth/domain/response/Oauth2Response.java
+++ src/main/java/com/verifit/verifit/auth/domain/response/Oauth2Response.java
@@ -1,7 +1,29 @@
 package com.verifit.verifit.auth.domain.response;
 
+import com.verifit.verifit.auth.domain.entity.Oauth2;
+import lombok.Builder;
+
+@Builder
 public record Oauth2Response(
         String redirect,
-        String accessToken
+        String accessToken,
+        String provider,
+        String providerId
 ) {
+    public static Oauth2Response login(String accessToken, Oauth2 oauth2){
+        return Oauth2Response.builder()
+                .redirect("/")
+                .accessToken(accessToken)
+                .provider(oauth2.getProvider())
+                .providerId(oauth2.getProviderId())
+                .build();
+    }
+
+    public static Oauth2Response signup(Oauth2 oauth2){
+        return Oauth2Response.builder()
+                .redirect("/signup")
+                .provider(oauth2.getProvider())
+                .providerId(oauth2.getProviderId())
+                .build();
+    }
 }
--- src/main/java/com/verifit/verifit/auth/service/AuthService.java
+++ src/main/java/com/verifit/verifit/auth/service/AuthService.java
@@ -1,15 +1,14 @@
 package com.verifit.verifit.auth.service;
 
+import com.verifit.verifit.auth.domain.entity.Oauth2;
 import com.verifit.verifit.auth.domain.response.Oauth2Response;
+import com.verifit.verifit.auth.repository.Oauth2Repository;
 import com.verifit.verifit.client.oauth2.domain.Oauth2UserInfo;
 import com.verifit.verifit.client.oauth2.service.Oauth2Service;
 import com.verifit.verifit.global.jwt.JwtProvider;
-import com.verifit.verifit.member.dao.MemberRepository;
-import com.verifit.verifit.member.entity.Member;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.StringUtils;
 
 @Service
 @RequiredArgsConstructor
@@ -18,24 +17,27 @@ public class AuthService {
 
     private final Oauth2Service oauth2Service;
     private final JwtProvider jwtProvider;
-    private final MemberRepository memberRepository;
+    private final Oauth2Repository oauth2Repository;
 
     @Transactional
     public Oauth2Response authenticate(String provider, String code) {
         Oauth2UserInfo userInfo = oauth2Service.getUserResource(provider, code);
 
-        Member member = findOauth2Member(userInfo.getProvider(), userInfo.getProviderId());
+        Oauth2 oauth2 = getOauth2ByProviderInfo(userInfo.getProvider(), userInfo.getProviderId());
 
-        return new Oauth2Response(getRedirect(member), jwtProvider.generateAccessToken(member));
+        return getOauth2Response(oauth2);
     }
 
-    private Member findOauth2Member(String provider, String providerId) {
-        return memberRepository
-                .findByProviderAndProviderId(provider, providerId)
-                .orElseGet(() -> memberRepository.save(Member.createOauthMember(provider, providerId)));
+    private Oauth2 getOauth2ByProviderInfo(String provider, String providerId) {
+        return oauth2Repository.findByProviderAndProviderId(provider, providerId)
+                .orElseGet(() -> oauth2Repository.save(Oauth2.createFirstLogin(provider, providerId)));
     }
 
-    private String getRedirect(Member member){
-        return StringUtils.hasText(member.getNickname()) ? "/signup" : "/";
+    private Oauth2Response getOauth2Response(Oauth2 oauth2) {
+        if (oauth2.checkMemberRegistration()) {
+            return Oauth2Response.login(jwtProvider.generateAccessToken(oauth2.getMember()), oauth2);
+        } else {
+            return Oauth2Response.signup(oauth2);
+        }
     }
 }
