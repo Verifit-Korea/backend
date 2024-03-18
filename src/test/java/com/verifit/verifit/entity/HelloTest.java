ed606485a986ac765d099bed562a247cf8fb3eee
mark1346 <markymark331@gmail.com>
2024-03-19 01:56:30 +0900

    create test for MypageService

--- src/test/java/com/verifit/verifit/entity/HelloTest.java
+++ src/test/java/com/verifit/verifit/entity/HelloTest.java
@@ -10,26 +10,26 @@ import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
-@Transactional
-class HelloTest { //Querydsl 테스트
-    @Autowired
-    EntityManager em;
-
-    @Test
-    void contextLoads() {
-        Hello hello = new Hello();
-        em.persist(hello);
-
-        JPAQueryFactory query = new JPAQueryFactory(em);
-        QHello qHello = QHello.hello; // Q타입 동작 확인
-
-        Hello result = query
-                .selectFrom(qHello)
-                .fetchOne();
-
-        Assertions.assertEquals(result, hello);
-        Assertions.assertEquals(result.getId(), hello.getId());
-    }
-
-}
+//@SpringBootTest
+//@Transactional
+//class HelloTest { //Querydsl 테스트
+//    @Autowired
+//    EntityManager em;
+//
+//    @Test
+//    void contextLoads() {
+//        Hello hello = new Hello();
+//        em.persist(hello);
+//
+//        JPAQueryFactory query = new JPAQueryFactory(em);
+//        QHello qHello = QHello.hello; // Q타입 동작 확인
+//
+//        Hello result = query
+//                .selectFrom(qHello)
+//                .fetchOne();
+//
+//        Assertions.assertEquals(result, hello);
+//        Assertions.assertEquals(result.getId(), hello.getId());
+//    }
+//
+//}
--- src/test/java/com/verifit/verifit/mypage/service/MypageServiceTest.java
+++ src/test/java/com/verifit/verifit/mypage/service/MypageServiceTest.java
@@ -1,4 +1,43 @@
+package com.verifit.verifit.mypage.service;
+
+import com.verifit.verifit.member.entity.Member;
+import com.verifit.verifit.member.service.MemberService;
+import com.verifit.verifit.mypage.dto.MemberInfoDTO;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+
 class MypageServiceTest {
-  
+
+    private MemberService memberServiceMock;
+    private MypageService mypageService;
+
+    @BeforeEach
+    public void setUp() {
+        memberServiceMock = mock(MemberService.class);
+        mypageService = new MypageService(memberServiceMock);
+    }
+
+    @Test
+    public void testGetMyInfo() {
+        // given
+        Member testMember = Member.createMember("testNickname",
+                "testEmail@gmail.com",
+                "testpassword",
+                "testProfileUrl");
+        // when
+        MemberInfoDTO resultDTO = mypageService.getMyInfoDTO(testMember);
+
+        // then
+        System.out.println("resultDTO = " + resultDTO);
+        assertEquals(resultDTO.getNickname(), "testNickname");
+        assertEquals(resultDTO.getEmail(), "testEmail@gmail.com");
+        assertEquals(resultDTO.getProfileUrl(), "testProfileUrl");
+    }
 }
